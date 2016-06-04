3e652e81d3471ee8592536aaae110473bb872b7a
Matthew Knoth <matthew.knoth@target.com>
2016-06-04 14:07:03 -0500

    Init check in of site

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('openBeds3')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();

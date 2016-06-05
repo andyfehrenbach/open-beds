6d1df6fa46af5a63097ca2a6f21f8fd497ae44aa
gfilet <phil0837@d.umn.edu>
2016-06-05 12:00:09 -0500

    Display map based on location

--- src/app/map/map.controller.js
+++ src/app/map/map.controller.js
@@ -6,15 +6,34 @@
     .controller('MapController', MapController);
   /** @ngInject */
   function MapController($http, $scope) {
+      var geocoder;
+      var lat; 
+      var lng;  
+      $.ajax({
+          url: "http://www.google.com/jsapi",
+          dataType:"script",
+          timeout: 2*10000,
+          async: true,
+      }).success(function() {
+          setTimeout(function() {
+                var geocoder = new google.maps.Geocoder(); 
+                lat = google.loader.ClientLocation.latitude;
+                lng = google.loader.ClientLocation.longitude;
+          }, 1000);
+       })
+       .error(function() {
+           
+       });
       $.ajax({
           url: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAPJrc3CMvMrJ-3R5Enzpab3Vr6XclpK1g",
           dataType:"script",
-          timeout: 2*10000
+          timeout: 2*10000,
+          async: true,
       }).success(function() {
           setTimeout(function() {
-               var mapOptions = {
-            zoom: 8,
-            center: new google.maps.LatLng(-34.397, 150.644),
+            var mapOptions = {
+            zoom: 10,
+            center: new google.maps.LatLng(lat, lng),
             mapTypeId: google.maps.MapTypeId.ROADMAP
           };
            map = new google.maps.Map(document.getElementById('map'),
